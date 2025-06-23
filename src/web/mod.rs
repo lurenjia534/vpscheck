bd1ce7dab42f74bf7ac182d166047e6765c6c16f
luren <lurenjia534@outlook.com>
2025-06-24 01:18:53 +0800

    Allow setting port

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -5,8 +5,12 @@ pub mod routes;
 pub use routes::{metrics, ws};
 
 pub async fn run() -> std::io::Result<()> {
+    let port: u16 = std::env::var("PORT")
+        .ok()
+        .and_then(|p: String| p.parse().ok())
+        .unwrap_or(8080);
     let server = HttpServer::new(|| App::new().service(metrics).service(ws))
-        .bind(("0.0.0.0", 8080))?;
+        .bind(("0.0.0.0", port))?;
     for addr in server.addrs() {
         println!("Web 服务器已启动，监听地址：http://{}", addr);
         println!("WebSocket 服务器已启动，监听地址：ws://{}", addr);
